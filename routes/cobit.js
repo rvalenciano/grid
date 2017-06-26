2d5ddd4ac6ed7229a468353f2afddaee9912d486
Randall Valenciano Fallas <ravf.226@gmail.com>
2017-06-26 05:47:32 -0600

    First Commit

---
+++ routes/cobit.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs"),
+  json;
+var appRoot = require('app-root-path');
+
+
+function readJsonFileSync(filepath, encoding) {
+  if (typeof (encoding) == 'undefined') {
+    encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
+
+function getConfig(file) {
+  var filepath = appRoot.toString() + '/' + file;
+  return readJsonFileSync(filepath);
+}
+
+/* GET COBIT json data. */
+
+router.get('/', function (req, res) {
+  cobit = getConfig('config/cobit.json');
+  res.json({
+    cobit
+  });
+});
+
+module.exports = router;
---
+++ routes/grid.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs"),
+  json;
+var appRoot = require('app-root-path');
+
+
+function readJsonFileSync(filepath, encoding) {
+  if (typeof (encoding) == 'undefined') {
+    encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
+
+function getConfig(file) {
+  var filepath = appRoot.toString() + '/' + file;
+  return readJsonFileSync(filepath);
+}
+
+/* GET home page. */
+
+router.get('/', function (req, res) {
+  var cobit = getConfig('config/cobit.json');
+  res.render('grid', {
+    cobit: cobit,
+    title: 'Grid'
+  });
+});
+
+module.exports = router;
